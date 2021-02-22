2e7ba8fba1a601a9d6e7f8e21d77d3b17c56f810
RodriguezOviedoCesar <crodriguezov@ucvvirtutal.edu.pe>
2021-02-21 19:37:04 -0500

    Cambios 2.0 en el select.php

--- controlador/select.php
+++ controlador/select.php
@@ -1,6 +1,7 @@
 <?php
 
-class Select{
+class Select
+{
     public string $data;
     public function __construct(string $data)
     {
@@ -13,8 +14,8 @@ class Select{
         // foreach($dato as $dat){
         //     echo $dat."<br>";
         // }
-        
-        try{
+
+        try {
             $sql = "SELECT * FROM $data ";
             $result = $mysqli->query($sql);
             // $id = $result->fetch_assoc();            
@@ -23,68 +24,70 @@ class Select{
             // for($i =0; $i<$result->fetch_assoc();$i++){ Esto lo podemos tomar en cuenta para la operación
             //     echo $i;
             // }
-            if($result){
-                if($result->num_rows>0){
-                    while($id = $result->fetch_assoc()){
+            if ($result) {
+                if ($result->num_rows > 0) {
+                    while ($id = $result->fetch_assoc()) {
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="../otros/bootstrap/css/bootstrap.min.css">
-</head>
-<body>
-    <table class="table">
-        <tr class="table-dark">
-            <?php
-                 foreach($parametros as $parametro){
-                    for($i = 0; $i<= $result->fetch_assoc();$i++){
-                        echo "<th>";
-                        echo $parametro;
-                        echo "</th>";
-                    }
-                }
-            
-            ?>
-        </tr>
-            <?php
+                            <!DOCTYPE html>
+                            <html lang="en">
 
-                for($i = 0; $i <= $result->fetch_assoc(); $i++){
-                    echo"<tr>";
-                    foreach($datos as $dat){
-                            echo "<td>";
-                            echo $id[$dat];
-                            echo "</td>";
-                    }
-                    echo"</tr>";
-                }
-                
-            ?>
-    </table>
-</body>
-<head>
-    <script type="text/javascript" src="../otros/bootstrap/jquery-3.5.1.min.js"></script>
-    <script type="text/javascript" src="../otros/bootstrap/js/bootstrap.min.js"></script>
-</head>
-</html>
+                            <head>
+                                <meta charset="UTF-8">
+                                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                                <title>Document</title>
+                                <link rel="stylesheet" href="../otros/bootstrap/css/bootstrap.min.css">
+                            </head>
+
+                            <body>
+                                <table class="table">
+
+                                    <tr class="table-dark">
+                                        <?php
+                                        foreach ($parametros as $parametro) {
+                                            for ($i = 0; $i <= 0; $i++) {
+                                                echo "<th>";
+                                                echo $parametro;
+                                                echo "</th>";
+                                            }
+                                        }
+
+                                        ?>
+                                    </tr>
+                                    <tr>
+                                        <?php
+                                        foreach ($datos as $dat) {
+                                            for ($i = 0; $i <=  0; $i++) {
+                                                echo "<td>";
+                                                    echo $id[$dat];
+                                                echo "</td>";
+                                            }
+                                        }
+                                        ?>
+                                    </tr>
+
+                                </table>
+                            </body>
+
+                            <head>
+                                <script type="text/javascript" src="../otros/bootstrap/jquery-3.5.1.min.js"></script>
+                                <script type="text/javascript" src="../otros/bootstrap/js/bootstrap.min.js"></script>
+                            </head>
+
+                            </html>
 
 
 <?php
-                            
+
                         
                     }
-
                 }
             }
-        }catch(Exception $errors){
-            echo "Excepcion número ".$errors;
+        } catch (Exception $errors) {
+            echo "Excepcion número " . $errors;
         }
     }
-
 }
 
 
