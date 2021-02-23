e35350ea652fcd0ce558b15b93c9f0c3f510c9b9
RodriguezOviedoCesar <crodriguezov@ucvvirtutal.edu.pe>
2021-02-23 10:47:58 -0500

    Select completo 23/02/2021

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
                     <img src="../Farmacia1/otros/imagenes/logo.png" alt="logo" title="logo" style="height: 50px;" width="100px">
-                    <a class="navbar-brand" href="/vista/contenido/index.php" style="margin-left:5px">Farmacia Choclito</a>
+                    <a class="navbar-brand" href="/vista/contenido/index.php" style="margin-left:5px">Farmacia Name</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
---
+++ view/producto.php
@@ -0,0 +1,13 @@
+<?php
+
+require '../controlador/select.php';
+
+$producto = new Select('producto');
+$datos = array('idproducto','nomprod', 'fechahoravenc','stock','presentacion','concentracion',
+'formafarmaceutica','registrosanitario','precionuit');
+$parametros = array('ID','Nombre','Vence','Stock','Presentación', 'Concentración', 'Comprimido','Registro',
+'Precio');
+$producto->Select1('producto',$datos , $parametros);
+
+
+?>
