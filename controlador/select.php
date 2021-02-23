c73e75570c3175a7573adc0ada2e437dadd749fc
RodriguezOviedoCesar <crodriguezov@ucvvirtutal.edu.pe>
2021-02-23 10:47:58 -0500

    Select completo 23/02/2021

--- controlador/select.php
+++ controlador/select.php
@@ -25,96 +25,104 @@ class Select
             <title>Document</title>
             <link rel="stylesheet" href="../otros/bootstrap/css/bootstrap.min.css">
             <link rel="stylesheet" href="../otros/iconos/css/all.css">
+            <link rel="stylesheet" href="../otros/css/doctores.css">
         </head>
 
         <body>
-            <table class="table">
+            <div id="contenedor">
+                <div id="contenedordoctor">
+                    <div id="img">
+                        <img class="img-fluid" src="../otros/imagenes/logo.png" alt="logo" title="logo">
+                    </div>
+                    <table class="table">
+                        <tr class="table-dark">
+                            <?php
+                            foreach ($parametros as $parametro) {
+                                for ($i = 0; $i <= 0; $i++) {
+                                    echo "<th>";
+                                    echo $parametro;
+                                    echo "</th>";
+                                }
+                            }
 
-                <tr class="table-dark">
-                    <?php
-                    foreach ($parametros as $parametro) {
-                        for ($i = 0; $i <= 0; $i++) {
-                            echo "<th>";
-                            echo $parametro;
-                            echo "</th>";
-                        }
-                    }
+                            ?>
+                        </tr>
+                        <?php
 
-                    ?>
-                </tr>
+                        try {
+                            $sql = "SELECT * FROM $data ";
+                            $result = $mysqli->query($sql);
+
+                            // $id = $result->fetch_assoc();            
+                            // $fila = $result->fetch_assoc();
+                            // echo "</div>";
+                            // for($i =0; $i<$result->fetch_assoc();$i++){ Esto lo podemos tomar en cuenta para la operación
+                            //     echo $i;
+                            // }
+                            if ($result) {
+                                if ($result->num_rows > 0) {
+                                    while ($id = $result->fetch_assoc()) {
+
+                        ?>
+                                        <tr>
+                                            <?php
+
+                                            foreach ($datos as $dat) {
+                                                echo "<td>";
+                                                echo $id[$dat];
+                                                echo "</td>";
+                                            }
+                                            ?>
+                                            <td style="border: none;">
+                                                <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+                                                    <a href="">
+                                                        <button class="btn btn-danger me-md-2" type="button">
+                                                            <i class="fas fa-trash"></i>
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                            </td>
+                                            <td style="border:none">
+                                                <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                                                    <a href="">
+                                                        <button class="btn btn-warning me-md-2" type="button">
+                                                            <i class="fas fa-user-edit"></i>
+                                                        </button>
+                                                    </a>
+                                                </div>
+                                            </td>
+
+                                        </tr>
                 <?php
 
-                try {
-                    $sql = "SELECT * FROM $data ";
-                    $result = $mysqli->query($sql);
-                    // $id = $result->fetch_assoc();            
-                    // $fila = $result->fetch_assoc();
-                    // echo "</div>";
-                    // for($i =0; $i<$result->fetch_assoc();$i++){ Esto lo podemos tomar en cuenta para la operación
-                    //     echo $i;
-                    // }
-                    if ($result) {
-                        if ($result->num_rows > 0) {
-                            while ($id = $result->fetch_assoc()) {
-                ?>
-                                <tr>
-                                    <?php
-                                    foreach ($datos as $dat) {
-
-                                        echo "<td>";
-                                        echo $id[$dat];
-                                        echo "</td>";
                                     }
-                                    ?>
-                                    <td style="border: none;">
-                                        <div class="d-grid gap-2 d-md-flex justify-content-md-center">
-                                            <a href="">
-                                                <button class="btn btn-danger me-md-2" type="button">
-                                                    <i class="fas fa-trash"></i>
-                                                </button>
-                                            </a>
-                                        </div>
-                                    </td>
-                                    <td style="border:none">
-                                        <div class="d-grid gap-2 d-md-flex justify-content-md-start">
-                                            <a href="">
-                                                <button class="btn btn-warning me-md-2" type="button">
-                                                    <i class="fas fa-user-edit"></i>
-                                                </button>
-                                            </a>
-                                        </div>
-                                    </td>
-
-                                </tr>
-            </table>
-            <?php
-
-
+                                }
                             }
+                        } catch (Exception $errors) {
+                            echo "Excepcion  " . $errors;
                         }
                     }
-                } catch (Exception $errors) {
-                    echo "Excepcion número " . $errors;
                 }
-            }
-        }
 
-            ?>
-            <div class="d-grid gap-2 d-md-flex justify-content-md-end" style="margin-right: 150px;">
-                <a href="">
-                    <button class="btn btn-success me-md-2" type="button">
-                        <span>Agregar
-                            <i class="fas fa-user-plus"></i>
-                        </span>
-                    </button>
-                </a>
-                <a href="../index.php">
-                    <button class="btn btn-primary" type="button">
-                        <span>Regresar
-                            <i class="fas fa-arrow-circle-left"></i>
-                        </span>
-                    </button>
-                </a>
+                ?>
+                    </table>
+                    <div style="position: absolute; margin-top:30px; margin-left:15px" class="d-grid gap-2 d-md-flex justify-content-md-end" >
+                        <a href="">
+                            <button class="btn btn-success me-md-2" type="button">
+                                <span>Agregar
+                                    <i class="fas fa-user-plus"></i>
+                                </span>
+                            </button>
+                        </a>
+                        <a href="../index.php">
+                            <button class="btn btn-primary" type="button">
+                                <span>Regresar
+                                    <i class="fas fa-arrow-circle-left"></i>
+                                </span>
+                            </button>
+                        </a>
+                    </div>
+                </div>
             </div>
         </body>
 
--- vista/empleado.php
+++ vista/empleado.php
@@ -6,5 +6,4 @@ $doctor = new Select('empleado');
 $datos = array('idempleado','idtipoestado', 'idcargoempleado','dni','nombres','direccion','telefono');
 $parametros = array('ID','Estado','Cargo','DNI','Nombre', 'Direccion', 'Telefono');
 $doctor->Select1('empleado',$datos , $parametros);
-
 ?>
